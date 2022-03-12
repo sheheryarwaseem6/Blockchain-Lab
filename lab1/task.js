39d346e9dd70f380884b6aea709521863cc4c4cf
Sheheryar Waseem <sherishaikh6@gmail.com>
2022-03-12 17:41:59 +0500

    task 1 done

--- lab1/task.js
+++ lab1/task.js
@@ -2,20 +2,54 @@ let stdobj = {
             name : "Hassam",
             class : "BS_CE",
             section : "C",
-            marks: [70,80],
-            attd : 75
+            marks: [70,80,40,60,90],
+            attd : 70
 }
 
 console.log(stdobj.name);
 console.log(stdobj.class);
 console.log(stdobj.section);
-if(stdobj.attd >=75){
-   if(stdobj.marks[0]>= 70 && stdobj.marks[0]<=79){
-       console.log("Subject # 1 -- Grade B")
-   }
-   if(stdobj.marks[1]>= 80 && stdobj.marks[1]<=89){
-       console.log("Subject # 2 -- Grade A")
-   }
-} else {
-    console.log("Short of Attendence!");
+if(stdobj.attd >= 75){
+
+    for(let i = 0; i< stdobj.marks.length; i ++){
+        if(stdobj.marks[i]>=50){
+
+            if(stdobj.marks[i]>= 50 && stdobj.marks[i] <= 59){
+
+                console.log("D Grade")
+
+
+            }
+            else if (stdobj.marks[i]>= 60 && stdobj.marks[i] <= 69){
+
+                console.log("C Grade")
+
+
+            }
+            else if (stdobj.marks[i]>= 70 && stdobj.marks[i] <= 79){
+
+                console.log("B Grade")
+
+
+            }
+            else if (stdobj.marks[i]>= 80 && stdobj.marks[i] <= 89){
+
+                console.log("A Grade")
+
+
+            }
+            else if (stdobj.marks[i]>= 90 && stdobj.marks[i] <= 100){
+
+                console.log("A1 Grade")
+
+
+            }
+            
+        }
+        else{
+            console.log("you're failed in a course")
+        }
+    }
+}else{
+    console.log("you're failed due to short of attendance")
 }
