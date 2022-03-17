8f250469ecfcd625bc1f232d5764a49730b7d9e4
Sheheryar Waseem <sherishaikh6@gmail.com>
2022-03-17 22:58:17 +0500

    lab3

--- Lab2 and 3/lab3.js
+++ Lab2 and 3/lab3.js
@@ -1,25 +1,25 @@
-//make 2 arrays and find second name of an array into another array
+make 2 arrays and find second name of an array into another array
 
 
-// let array1 = ["Sheheryar Waseem", "Abdal Shakir", "Hamza Ilyas"]
-// let array2=["Shakir", "Ilyas","Waseem"]
-// let newArray;
+let array1 = ["Sheheryar Waseem", "Abdal Shakir", "Hamza Ilyas"]
+let array2=["Shakir", "Ilyas","Waseem"]
+let newArray;
 
-// array1.forEach((element, index) => {
-//     array2.forEach((item)=>{
-//         newArray = element.split(" ")
-//         if (newArray[1] === item) {
-//             console.log(`${item} is the father name of ${element} found at the indes number ${index}`)
+array1.forEach((element, index) => {
+    array2.forEach((item)=>{
+        newArray = element.split(" ")
+        if (newArray[1] === item) {
+            console.log(`${item} is the father name of ${element} found at the indes number ${index}`)
             
-//         }
+        }
 
         
-//     })
+    })
     
-// });
+});
 
 
-// 2d array and loop
+2d array and loop
 
 students=["sheheryar","hamza","abdal"]
 subjects=["maths","physics","chemistry","computer"]
